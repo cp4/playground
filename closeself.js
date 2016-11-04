5b70e62d6c6b3c83d23babbba9da110a05ac1741
Max Wang <wangppei@gmail.com>
2016-11-04 21:13:22 +0800

    Create closeself.js

---
+++ closeself.js
@@ -0,0 +1,22 @@
+nihao = "你好世界"
+alert(nihao);
+
+function disp_prompt()
+  {
+    var name=prompt("请问你叫什么名字？")
+    if (name != null && name != "")
+      {
+        name += "，你好"
+        document.write(name)
+      }
+  }
+function close_win()
+{
+if (window.confirm("是否关闭页面"))
+  {
+  alert("nihao")
+   window.opener=null;
+   window.open('','_self');
+   window.close();
+  }
+}
